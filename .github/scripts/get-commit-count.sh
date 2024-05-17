1c7eee95201f235ede53003d343ec5b31c2a5ec7
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-18 02:34:05 +0700

    move commit-count to .github

--- .github/scripts/get-commit-count.sh
+++ .github/scripts/get-commit-count.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -f "commit-count" ]; then
-    OLD_COMMIT_COUNT=$(cat commit-count)
+if [ -f "./github/commit-count" ]; then
+    OLD_COMMIT_COUNT=$(cat ./github/commit-count)
 else
     OLD_COMMIT_COUNT=0
-    echo "$OLD_COMMIT_COUNT" >commit-count
+    echo "$OLD_COMMIT_COUNT" >./github/commit-count
 fi
 
 NEW_COMMIT_COUNT=$(curl -I -k "https://api.github.com/repos/zrax/pycdc/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p')
 if [ "$NEW_COMMIT_COUNT" -gt "$OLD_COMMIT_COUNT" ]; then
-    echo "$NEW_COMMIT_COUNT" >commit-count
+    echo "$NEW_COMMIT_COUNT" >./github/commit-count
     echo "True"
     exit 0
 else
