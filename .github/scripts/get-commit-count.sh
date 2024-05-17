26110998d9474f0fc1ac1853eb42f2bad520a8a4
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-18 02:17:14 +0700

    test action

---
+++ .github/scripts/get-commit-count.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -f "commit-count" ]; then
+    OLD_COMMIT_COUNT=$(cat commit-count)
+else
+    OLD_COMMIT_COUNT=0
+    echo "$OLD_COMMIT_COUNT" >commit-count
+fi
+
+NEW_COMMIT_COUNT=$(curl -I -k "https://api.github.com/repos/zrax/pycdc/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p')
+if [ "$NEW_COMMIT_COUNT" -gt "$OLD_COMMIT_COUNT" ]; then
+    echo "$NEW_COMMIT_COUNT" >commit-count
+    echo "True"
+    exit 0
+else
+    echo "False"
+    exit 1
+fi
